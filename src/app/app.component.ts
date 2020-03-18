a5189e32025af99b78e04fbb644d888c25325fe8
amaraneniakash <amaraneniakash561@gmail.com>
2020-03-18 19:31:54 -0400

    Yaml for deploying into kubectl & default data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,30 @@ export class AppComponent implements OnInit {
       if (res) {
         this.chartData = res;
       }
+    }, err => {
+      if (err) {
+        this.chartData = [{
+          name: 'Speedometer Chart',
+          type: 'speedometer',
+          data: '60',
+        }, {
+          name: 'Progress Bar Chart',
+          type: 'progressBar',
+          data: '40',
+        }, {
+          name: 'Donut Chart',
+          type: 'donut',
+          data: '[{ "name": "Pen", "value": 40 }, { "name": "Pencil", "value": 60 }]',
+        }, {
+          name: 'Vertical Bar Chart',
+          type: 'barChart',
+          data: `[{ "name": "Q4 FY15", "value": 70 },{ "name": "Q1 FY16", "value": 30 },
+          { "name": "Q2 FY16", "value": 50 },{ "name": "Q3 FY16", "value": 45 },
+          { "name": "Q4 FY16", "value": 25 },{ "name": "Q1 FY17", "value": 15 },
+          { "name": "Q2 FY17", "value": 85 }, { "name": "Q3 FY17", "value": 85 },
+          { "name": "Q4 FY17", "value": 25 },{ "name": "Q1 FY18", "value": 95 }]`,
+        }];
+      }
     });
   }
 }
